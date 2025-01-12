d712fabc288377ec0d734eb91f3181294e7ab5e1
asadmukhtarr <axad03213@gmail.com>
2025-01-12 19:55:39 +0500

    php installation & php project create & php files creation

--- README.md
+++ README.md
@@ -12,7 +12,26 @@
         - Settings
         - Logout ..
 # Course Topic Scheduale ..
-    - PHP Base ( Login System , CRUD System)...
+    - PHP Base ( Login System , CRUD System)... (Login , Forget Password, Register , Home) ..
     - Mysql ( DBMS )..
     - Laravel ( Clinical Management System) ..
     - React ( Website ) ...
+# Guidelines
+    - Types of servers:
+	- Live Server : 
+          We can access on live server, Any where in the world.
+	  Namecheap , Bluehost , Godddy  ...
+	  - Name of your website: domain ..
+          - Storage of your website: Hosting ...
+	- Local Server : We can access it only in computer for practice.
+    - Steps For Install Php
+        - Step # 1: Xampp ( Suggestion) / Wampp .. Download ...
+        - Step # 2: Create Your First Project: .. 
+            C:xampp/htdocs/{Your Project} ..
+    - Require  ( Need ) .. Mostly For Backend Functoinality ...
+    - Include (Shamil Krna ) ..  / inlucde_once(); ...
+
+# Tips And Tricks
+    - We can write any code related to web in php .. ( Html , JS , PHP , CSS) ...
+# Helping Links
+    - https://www.youtube.com/watch?v=SMhcOUwRCt8&list=PLPvf-1cCig-HVjByU6jLou-IRlGpsGA61
---
+++ phppractice/includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Login System</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-warning">
+        <div class="container">
+            <a class="navbar-brand text-white" href="home.php"> <i class="fa fa-shield" aria-hidden="true"></i> Security System</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link text-white" aria-current="page" href="home.php"> <i class="fa fa-home"></i> Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-user-circle-o" aria-hidden="true"></i> My Acount
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="index.php">Login </a></li>
+                    <li><a class="dropdown-item" href="register.php">Register</a></li>
+                </ul>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
