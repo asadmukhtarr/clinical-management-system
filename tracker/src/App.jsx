6a0b572e281c3ca065d947dc9c8446f72b7b0c1b
asadmukhtarr <axad03213@gmail.com>
2025-04-12 19:46:50 +0500

    react component

---
+++ tracker/src/App.jsx
@@ -0,0 +1,23 @@
+import Home from "./components/Home";
+import About from "./components/About";
+import Contct from "./components/Contact";
+import Doctors from "./components/Doctors";
+import Appointments from "./components/Appointments";
+import Book from "./components/Book";
+import Track from "./components/Track";
+const App = () => {
+    return (
+        <div>
+            <h2>Welcome to main component</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos rem saepe facere quasi expedita pariatur repellat ipsum, nostrum quos eveniet explicabo eius fuga nesciunt magni, ut voluptates consequatur. Dolorum illum magnam, sit, suscipit eius iste debitis quam quaerat voluptas, nobis nostrum amet porro? Quod reiciendis, quos, delectus eveniet odit adipisci, quas ad laudantium veniam molestias velit iure placeat? Sed error quisquam adipisci ad, esse laborum ipsa officia odit a quos amet. Totam soluta quisquam velit, repudiandae ducimus voluptatibus voluptates minus sequi? Eum, quis! Consequuntur repudiandae velit ullam laboriosam nostrum fuga! Dolorum doloremque ea necessitatibus eum laudantium earum? Eaque autem aut eveniet natus iusto dolores repellat assumenda beatae qui quisquam odit, ut hic officiis nisi consequatur? Autem incidunt quaerat quasi laboriosam libero. Tempore assumenda autem ut excepturi nulla mollitia quibusdam praesentium, repellendus similique doloribus suscipit dolor obcaecati aliquam, quos enim vel commodi sunt odit porro minima. Autem molestias debitis saepe necessitatibus. Possimus, itaque modi at, beatae asperiores culpa nobis qui aspernatur debitis sunt officiis numquam eius vitae, laudantium alias delectus saepe magnam! Aspernatur, suscipit asperiores quae, dolorum laudantium aliquid ut laboriosam illum minus provident nesciunt consectetur. Obcaecati laudantium commodi facere modi mollitia a? Tenetur blanditiis voluptate officiis modi, atque odio fugit.</p>
+            <Home />
+            <About />
+            <Contct />
+            <Doctors />
+            <Appointments />
+            <Book />
+            <Track />
+        </div>
+    )
+}
+export default App;
