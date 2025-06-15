35cadf1e392e372a3219d3559f8029f1d7f5eff9
asadmukhtarr <axad03213@gmail.com>
2025-06-15 19:54:02 +0500

    mongo connection + fetch data from mongo

--- asadmukhtarnode/index.js
+++ asadmukhtarnode/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const app = express();
 const path = require('path');
 const port = 3030;
+
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+const dbName = "zainab";
+let db;
+
+// Connect to MongoDB
+async function mongodb_connection() {
+    try {
+        const client = await MongoClient.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        db = client.db(dbName);
+        console.log("✅ Database Connected Successfully - Asad");
+    } catch (error) {
+        console.error("❌ Can't connect with DB", error);
+    }
+}
+mongodb_connection();
+
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
@@ -16,6 +38,16 @@ app.get('/contact',(req,res)=> {
 });
 app.get('/products',(req,res)=> {
     res.render('products');
+});
+app.get('/create', async (req, res) => {
+    try {
+        const collection = db.collection("products");
+        const products = await collection.find({}).toArray();
+        //res.json(products);
+        res.render('create',{products}); 
+    } catch (error) {
+        console.log("cant fetch data from mongodb", error);
+    }
 });
 app.listen(port,()=> {
     console.log('Hello Dost Project Is Running On ',port);
